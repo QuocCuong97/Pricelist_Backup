9d09f97b8955cdf130a816cd36ef3c56d17c63a7
QuocCuong97 <cuongnq24101997@gmail.com>
2020-02-17 12:27:55 +0700

    12:27

--- project/home/views.py
+++ project/home/views.py
@@ -4,8 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/home.html')
+    return render(request, 'pages/domain.html')
 
+def hosting(request):
+    return render(request, 'pages/hosting.html')
+
+def vps(request):
+    return render(request, 'pages/vps.html')
 # def list(request):
 #     Data = {"Vendor": Vendor.objects.all()}
 #     return render(request, 'pages/home.html', Data)
