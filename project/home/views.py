00156756d5433a4632fedaa92fbb24cdb5b935e6
QuocCuong97 <cuongnq24101997@gmail.com>
2020-02-17 23:44:11 +0700

    23:44

--- project/home/views.py
+++ project/home/views.py
@@ -14,12 +14,12 @@ def vps(request):
     return render(request, 'pages/vps.html')
 
 def index(request):
-    lst_vn = Domain.objects.all().filter(domain_type='vn').order_by('sale_price').values_list('sale_price')
-    lst_comvn = Domain.objects.all().filter(domain_type='comvn').order_by('sale_price').values_list('sale_price')
-    lst_com = Domain.objects.all().filter(domain_type='com').order_by('sale_price').values_list('sale_price')
-    lst_net = Domain.objects.all().filter(domain_type='net').order_by('sale_price').values_list('sale_price')
-    lst_org = Domain.objects.all().filter(domain_type='org').order_by('sale_price').values_list('sale_price')
-    lst_info = Domain.objects.all().filter(domain_type='info').order_by('sale_price').values_list('sale_price')
+    lst_vn = Domain.objects.all().filter(domain_type='vn').order_by('sale_price').values_list('sale_price', 'vendor__name')
+    lst_comvn = Domain.objects.all().filter(domain_type='comvn').order_by('sale_price').values_list('sale_price', 'vendor__name')
+    lst_com = Domain.objects.all().filter(domain_type='com').order_by('sale_price').values_list('sale_price', 'vendor__name')
+    lst_net = Domain.objects.all().filter(domain_type='net').order_by('sale_price').values_list('sale_price', 'vendor__name')
+    lst_org = Domain.objects.all().filter(domain_type='org').order_by('sale_price').values_list('sale_price', 'vendor__name')
+    lst_info = Domain.objects.all().filter(domain_type='info').order_by('sale_price').values_list('sale_price', 'vendor__name')
 
     lst_cheapest = [lst_com[0], lst_net[0], lst_org[0], lst_info[0], lst_comvn[0], lst_vn[0]]
     lst_second = [lst_com[1], lst_net[1], lst_org[1], lst_info[1], lst_comvn[1], lst_vn[1]]
